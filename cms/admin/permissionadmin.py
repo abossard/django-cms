abossard/django-cms

commit 7715bc76648876d1a0c55109370b261ac203ae63

Get rid of unneeded BaseInlineFormSetWithQuerySet and use the existing hooks

cms/admin/permissionadmin.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
 from django.conf import settings
-from cms.admin.models import BaseInlineFormSetWithQuerySet
 from django.template.defaultfilters import title
 from django.utils.translation import ugettext as _
 
@@ -32,42 +31,33 @@ class PagePermissionInlineAdmin(admin.TabularInline):
         qs = PagePermission.objects.subordinate_to_user(request.user)
         return qs
     
-    def get_fieldsets(self, request, obj=None):
-        """Request formset with given obj.
-        """
-        if self.declared_fieldsets:
-            return self.declared_fieldsets
-        form = self.get_formset(request, obj).form
-        return [(None, {'fields': form.base_fields.keys()})]
-    
     def get_formset(self, request, obj=None, **kwargs):
-        """Some fields may be excluded here. User can change only 
-        permissions which are available for him. E.g. if user does not haves 
-        can_publish flag, he can't change assign can_publish permissions.
-        
-        Seems django doesn't cares about queryset defined here - its
-        probably a bug, so monkey patching again.. Assign use_queryset
-        attribute to FormSet, our overiden formset knows how to handle this, 
-        @see BaseInlineFormSetWithQuerySet for more details.
         """
+        Some fields may be excluded here. User can change only
+        permissions which are available for him. E.g. if user does not haves
+        can_publish flag, he can't change assign can_publish permissions.
+        """
+        exclude = self.exclude or []
         if obj:
-            self.exclude = []
             if not obj.has_add_permission(request):
-                self.exclude.append('can_add')
+                exclude.append('can_add')
             if not obj.has_delete_permission(request):
-                self.exclude.append('can_delete')
+                exclude.append('can_delete')
             if not obj.has_publish_permission(request):
-                self.exclude.append('can_publish')
+                exclude.append('can_publish')
             if not obj.has_advanced_settings_permission(request):
-                self.exclude.append('can_change_advanced_settings')
+                exclude.append('can_change_advanced_settings')
             if not obj.has_move_page_permission(request):
-                self.exclude.append('can_move_page')
+                exclude.append('can_move_page')
             if not settings.CMS_MODERATOR or not obj.has_moderate_permission(request):
-                self.exclude.append('can_moderate')
-        FormSet = super(PagePermissionInlineAdmin, self).get_formset(request, obj=None, **kwargs)
-        # asign queryset 
-        FormSet.use_queryset = self.queryset(request)
-        return FormSet
+                exclude.append('can_moderate')
+        formset_cls = super(PagePermissionInlineAdmin, self
+            ).get_formset(request, obj=None, exclude=exclude, *kwargs)
+        qs = self.queryset(request)
+        if obj is not None:
+            qs = qs.filter(page=obj)
+        formset_cls._queryset = qs
+        return formset_cls
 
 if settings.CMS_PERMISSION: 
     PAGE_ADMIN_INLINES.append(PagePermissionInlineAdmin)

cms/admin/models.py
@@ -1,23 +1,0 @@
-from django.forms.models import BaseInlineFormSet
-
-class BaseInlineFormSetWithQuerySet(BaseInlineFormSet):
-    """Overriden BaseInlineFormSet, so we can pass queryset to it instead of
-    _default_manager, see django bug #11019 for more details.
-    """
-    def __init__(self, data=None, files=None, instance=None,
-                 save_as_new=False, prefix=None, queryset=None):
-        from django.db.models.fields.related import RelatedObject
-        if instance is None:
-            self.instance = self.model()
-        else:
-            self.instance = instance
-        self.save_as_new = save_as_new
-        # is there a better way to get the object descriptor?
-        self.rel_name = RelatedObject(self.fk.rel.to, self.model, self.fk).get_accessor_name()
-        if hasattr(self, 'use_queryset'):
-            qs = self.use_queryset 
-        else:
-            qs = self.model._default_manager
-        qs = qs.filter(**{self.fk.name: self.instance})
-        super(BaseInlineFormSet, self).__init__(data, files, prefix=prefix or self.rel_name,
-                                                queryset=qs)

commit bc3df45d923ca57ac680f50576a16a9c1271f7f7

Fix typo introduced in 7715bc7.
This was stopping kwargs from being passed correctly
to a superclass.

cms/admin/permissionadmin.py
@@ -70,7 +70,7 @@ class PagePermissionInlineAdmin(TabularInline):
             if not obj.has_move_page_permission(request):
                 exclude.append('can_move_page')
         formset_cls = super(PagePermissionInlineAdmin, self
-        ).get_formset(request, obj=None, exclude=exclude, *kwargs)
+        ).get_formset(request, obj=None, exclude=exclude, **kwargs)
         qs = self.queryset(request)
         if obj is not None:
             qs = qs.filter(page=obj)